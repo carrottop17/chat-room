1bda15cd15ad5b7fb34c99bb1b894a36f60cdb5e
Daniel Barranco <danielbarranco@gmail.com>
2016-08-31 16:37:21 -0400

    made it a dictionary game

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var http = require("http");
 var fs = require("fs");
 
-
-//server is what happens when someone loads up the page in a browser.  server is listening below for http traffic at port xxxx.
+//server is the what happens when someone loads up the page in a browser.
+//server is listening below for http traffic at port XXXX
 var server = http.createServer(function(req, res){
-	console.log('someone connected via http');
+
+
+	if(req.url === '/styles.css'){
+		// fs.readFile('styles.css', 'utf-8', function(error, data){
+		// 	// console.log(error);
+		// 	// console.log(data);
+		// 	if(error){
+		// 		res.writeHead(500, {'content-type': 'text/html'});
+		// 		res.end(error);
+		// 	}else{
+		// 		res.writeHead(200, {'content-type': 'text/html'});
+		// 		res.end(data);
+		// 	});
+	}
+
+	console.log("Someone connected via http");
 	fs.readFile('index.html', 'utf-8', function(error, data){
 		// console.log(error);
 		// console.log(data);
@@ -15,38 +30,59 @@ var server = http.createServer(function(req, res){
 			res.writeHead(200, {'content-type': 'text/html'});
 			res.end(data);
 		}
-		
 	});
 });
 
-//include the socketio module
+//Include the socketio module
 var socketIo = require('socket.io');
-//listen to the server which is listening on port XXXX
+// listen to the server which is listening on port XXXX
 var io = socketIo.listen(server);
 var socketUsers = [];
-//we need to deal with a new socket connection
+
+//We need to deal wiht a new socket connection
 io.sockets.on('connect', function(socket){
-	socketUsers.push(socket);
-	console.log('someone connected via socket');
+	// console.log(socket);
+	socketUsers.push({
+		socketID: socket.id,
+		name: 'Anonymous'
+	});
+
+	io.sockets.emit('users',socketUsers);	
+
+	// console.log(socket);
+	console.log("Someone connected via a socket!");
+
+	//Someone just changed their name.
+	socket.on('name_to_server', function(name){
+		for(var i = 0; i<socketUsers.length; i++){
+			if(socketUsers[i].socketID == socket.id){
+				socketUsers[i].name = name;
+				break;
+			}
+		}
+		io.sockets.emit('users',socketUsers);	
+	});
 	socket.on('message_to_server', function(data){
-		io.sockets.emit('message_to_client', {
+		io.sockets.emit('message_to_client',{
 			message: data.message,
 			name: data.name,
 			date: data.date
 		});
 	});
-	socket.on('message_to_server2', function(data){
-		io.sockets.emit('message_to_client2', {
-			name: data.name,
-		});
-	});
 	socket.on('disconnect', function(){
-		console.log('a user has dissconnected');
-		var user = socketUsers.indexOf(socket);
-		socketUsers.splice(user,1);
+		console.log(socket.id + " -- user has disconnected");
+		for(var i = 0; i<socketUsers.length; i++){
+			if(socketUsers[i].socketID == socket.id){
+				socketUsers.splice(i, 1);
+				break;
+			}
+		}
+		io.sockets.emit('users',socketUsers);	
 	});
+
 });
 
 server.listen(8080);
-console.log("Listening on port 8080");
+console.log('Listening on Port 8080.');
+
 
